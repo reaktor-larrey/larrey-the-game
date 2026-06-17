9ddb069598c9461e6487ba3110ed435b1bbe9565
Stephen Buchanan <stephen.buchanan@reaktor.com>
2026-06-17 13:23:54 +0200

    tidy up unused

--- web/src/lib/pkg/external.ts
+++ web/src/lib/pkg/external.ts
@@ -1,10 +1,8 @@
 import { goto } from '$app/navigation';
 import { resolve } from '$app/paths';
-import { leaderboardServiceInstance } from '$lib/services/leaderboard.service';
 
 export async function submit_score(score: number) {
 	console.log('should submit score', score, typeof score);
 
 	await goto(resolve('/enter/[score]', { score: score.toString() }));
-	// await leaderboardServiceInstance.updateScore('testonly', score);
 }
