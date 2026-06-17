fcfa71b610ca52fec3cec4bf8bfe76685af15c34
Stephen Buchanan <stephen.buchanan@reaktor.com>
2026-06-17 13:23:37 +0200

    super-insecure score submission works!

--- web/src/lib/pkg/external.ts
+++ web/src/lib/pkg/external.ts
@@ -1,7 +1,10 @@
+import { goto } from '$app/navigation';
+import { resolve } from '$app/paths';
 import { leaderboardServiceInstance } from '$lib/services/leaderboard.service';
 
 export async function submit_score(score: number) {
 	console.log('should submit score', score, typeof score);
 
-	await leaderboardServiceInstance.updateScore('testonly', score);
+	await goto(resolve('/enter/[score]', { score: score.toString() }));
+	// await leaderboardServiceInstance.updateScore('testonly', score);
 }
